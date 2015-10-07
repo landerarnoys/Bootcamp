bc753204dc464ed1f307e6fdbdc6a46d027d7064
landerarnoys <lander@Landers-MacBook-Pro.local>
2015-10-07 12:11:25 +0200

    exercise two

---
+++ Week2/Exercises/Module/scripts/App.js
@@ -0,0 +1,28 @@
+(function ($,MyCalc) {
+
+     $( "#sum" ).click(function() {
+        var x = $('#numberOne').val();
+        var y = $('#numberTwo').val();
+         output(MyCalc.sum(x,y));
+    });
+
+
+      $( "#multiply" ).click(function() {
+        var x = $('#numberOne').val();
+        var y = $('#numberTwo').val();
+
+        output(MyCalc.multiply(x,y));
+       });
+
+      function output(value){
+        console.log(value);
+        $("#output").text( value );
+
+      }
+
+})(jQuery, MyCalc);
+
+
+
+
+
---
+++ Week2/javascriptConcepts.js
@@ -0,0 +1,14 @@
+
+var module = (function(){
+
+        'use strict'
+        var publicAPI = {};
+
+        publicAPI.method = function(){
+
+        };
+
+        window.publicAPI;
+
+
+})()
